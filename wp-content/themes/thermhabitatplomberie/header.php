d5c402e6c4f465decd05ae190645f4ad5ea901e6
Sebastien Houssein <housseinseb@icloud.com>
2025-09-15 16:18:08 +0200

    Adding header

--- wp-content/themes/thermhabitatplomberie/header.php
+++ wp-content/themes/thermhabitatplomberie/header.php
@@ -12,3 +12,34 @@
 
 </head>
 <body <?php body_class(); ?>>
+
+    <?php
+    $logo = get_field('logo-site', 'option') ? get_field('logo-site', 'option')['ID'] : get_theme_mod('custom_logo');
+    ?>
+
+    <header id="header-site">
+
+        <div class="container">
+
+            <?php if ($logo || $logoMobile): ?>
+                <a href="<?php echo get_home_url(); ?>" class="logo-container">
+                    <?php echo wp_get_attachment_image($logo, 'full'); ?>
+                </a>
+            <?php endif; ?>
+
+
+            <?php
+            wp_nav_menu([
+                'theme_location' => 'main_navigation',
+                'container_class' => 'main-navigation',
+                'menu_class' => 'main-menu',
+                'items_wrap' => '<ul class="%2$s">%3$s</ul>',
+            ]);
+            ?>
+
+            <?php get_template_part('template-parts/elements/burger'); ?>
+
+
+        </div>
+
+    </header>
